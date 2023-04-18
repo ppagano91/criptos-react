493fd7a017851aa4961f481557997417035899c8
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-18 00:29:02 -0300

    Agrego SelectCriptomonedas con hook useSelectMoneda

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -25,6 +25,10 @@ const InputSubmit = styled.input`
 const Formulario = () => {
   const [criptos, setCriptos] = useState([]);
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+  const [criptomoneda, SelectCriptomoneda] = useSelectMonedas(
+    "Elige tu Criptomoneda",
+    criptos
+  );
 
   useEffect(() => {
     const consultarAPI = async () => {
@@ -49,6 +53,7 @@ const Formulario = () => {
   return (
     <form>
       <SelectMonedas />
+      <SelectCriptomoneda />
       <InputSubmit type="submit" value="Cotizar" />
     </form>
   );
