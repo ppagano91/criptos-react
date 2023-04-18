33ecf54c0e9a66ab31ddc6f3faf8da1909c88216
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-04-18 00:37:10 -0300

    Validando Formulario + Componente Error + styled Componente Texto

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
+import Error from "./Error";
 
 const InputSubmit = styled.input`
   background-color: #9497ff;
@@ -24,6 +25,7 @@ const InputSubmit = styled.input`
 `;
 const Formulario = () => {
   const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
   const [criptomoneda, SelectCriptomoneda] = useSelectMonedas(
     "Elige tu Criptomoneda",
@@ -49,13 +51,24 @@ const Formulario = () => {
     };
     consultarAPI();
   }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+      return;
+    }
+    setError(false);
+  };
 
   return (
-    <form>
-      <SelectMonedas />
-      <SelectCriptomoneda />
-      <InputSubmit type="submit" value="Cotizar" />
-    </form>
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptomoneda />
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
   );
 };
 
